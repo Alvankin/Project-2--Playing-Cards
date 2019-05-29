b018a36fa603c428fc64b0ab49463b6d3652adce
Alvankin <49323226+Alvankin@users.noreply.github.com>
2019-05-29 17:48:37 +0200

    Update Code-Project2-Playing Cards.R

--- Code-Project2-Playing Cards.R
+++ Code-Project2-Playing Cards.R
@@ -105,3 +105,97 @@ head(deck4)
 
 random <- sample(1:52, size = 52)
 deck4 <- deck[random, ]
+
+print(shuffle(deck))
+
+deal(deck)
+deck2 <- shuffle(deck)
+deal(deck2)
+
+#6.4 Dollar Signs and Double Brackets----
+deck$value
+mean(deck$value)
+median(deck$value)
+lst <- list(numbers = c(1, 2), logical = TRUE, strings = c("a", "b", "c"))
+lst
+lst[1]
+sum(lst$numbers)
+sum(lst[[1]])
+
+#7Modifying Values----
+deck2 <- deck
+
+#7.0.1 Changing Values in Place----
+vec <- c(0, 0, 0, 0, 0, 0)
+typeof(vec)
+class(vec)
+vec[1]
+vec[1] <- 1000
+vec
+vec[c(1, 3, 5)] <- c(1, 1, 1)
+vec
+vec[4:6] <- vec[4:6] + 1
+vec
+vec[7] <- 0
+vec
+deck2$new <- 1:52
+head(deck2)
+deck2$new <- NULL
+head(deck2)
+deck2[c(13, 26, 39, 52), ]
+deck2[c(13, 26, 39, 52), 3]
+deck2$value[c(13, 26, 39, 52)]
+deck2$value[c(13, 26, 39, 52)] <- 14
+head(deck2, 13)
+deck3 <- shuffle(deck)
+head(deck3)
+
+#7.0.2 Logical Subsetting
+#7.0.2.1 Logical Tests
+deck2$face
+deck2$face == "ace"
+sum(deck2$face == "ace")
+deck3$face == "ace"
+deck3$value[deck3$face == "ace"]
+deck3$value[deck3$face == "ace"] <- 14
+head(deck3)
+deck4 <- deck
+deck4$value <- 0
+head(deck4, 13)
+deck4$suit == "hearts"
+deck4$value[deck4$suit =="hearts"] <- 1
+#7.0.2.2 Boolean Operators----
+queenOfSpades <- deck4$face == "queen" & deck4$suit == "spades"
+deck4$value[queenOfSpades] <- 13
+w <- c(-1, 0, 1)
+x <- c(5, 15)
+y <- "February"
+z <- c("Monday", "Tuesday", "Friday")
+w > 0
+x > 10 & x < 20
+y == "February"
+all(z %in% c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", 
+             "Saturday", "Sunday"))
+deck5 <- deck
+head(deck5)
+facecard <- deck5$face %in% c("king", "queen", "jack")
+deck5[facecard,]
+deck5$value[facecard] <- 10
+head(deck5, 13)
+
+#7.0.3.1 na.rm----
+c(NA, 1:50)
+mean(c(NA, 1:50))
+mean(c(NA, 1:50), na.rm = TRUE)
+
+#7.0.3.2 is.na----
+vec <- c(1, 2, 3, NA)
+is.na(vec)
+deck5$value[deck5$face =="ace"] <- NA
+head(deck5, 13)
+
+#8 Environments----
+#8.1 Environments----
+library(pryr)
+parenvs(all = TRUE)
+#To be read further
